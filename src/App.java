ecd4c80bf40aa98d76e73a2d50e95494386648fd
Simon Nestler <simon.nestler@thi.de>
2025-03-20 11:38:05 +0100

    statische methode getcount

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ public class App {
         Widget w1 = new Widget(10, 20);
         Widget w2 = new Widget(30, 40);
 
-        System.out.println(w1.getX());
+        System.out.println(Widget.getCount());
     }
 }
--- src/Widget.java
+++ src/Widget.java
@@ -2,14 +2,20 @@ public class Widget {
 
     private int x;
     private int y;
+    private static int count = 0;
 
     public Widget(int x, int y) {
         this.x = x;
         this.y = y;
-        System.out.println("Widget created at (" + x + ", " + y + ")");
+        count++;
+        System.out.println("Widget " + count + " created.");
     }
 
     public int getX() {
         return x;
+    }
+
+    public static int getCount() {
+        return count;
     }
 }
