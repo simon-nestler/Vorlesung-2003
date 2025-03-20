a4b40ee23fea5d6ea01e0049da235bd8cfd164fe
Simon Nestler <simon.nestler@thi.de>
2025-03-20 11:24:45 +0100

    x und y für alle objekte

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Widget w1 = new Widget(10, 20);
         Widget w2 = new Widget(30, 40);
+
+        System.out.println(w1.getX());
     }
 }
--- src/Widget.java
+++ src/Widget.java
@@ -1,6 +1,15 @@
 public class Widget {
 
+    private int x;
+    private int y;
+
     public Widget(int x, int y) {
+        this.x = x;
+        this.y = y;
         System.out.println("Widget created at (" + x + ", " + y + ")");
+    }
+
+    public int getX() {
+        return x;
     }
 }
