77ef3af714c4a3bdfe47036542c50117fcfaa05d
Simon Nestler <simon.nestler@thi.de>
2025-03-20 12:08:09 +0100

    Copy-Konstruktor

--- src/App.java
+++ src/App.java
@@ -1,8 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Widget w1 = new Widget(10, 20);
-        Widget w2 = new Widget(30, 40);
+        // Widget w1 = new Widget(10, 20);
+        // Widget w2 = new Widget(30, 40);
 
-        System.out.println(Widget.getCount());
+        // Widget w1 = new Widget(10, 20);
+        // Widget w2 = w1;
+
+        Widget w1 = new Widget(10, 20);
+        Widget w2 = new Widget(w1);
+        w2.setX(20);
+
+        System.out.println(w1.getX() + " " + w2.getX());
     }
 }
--- src/Widget.java
+++ src/Widget.java
@@ -2,20 +2,23 @@ public class Widget {
 
     private int x;
     private int y;
-    private static int count = 0;
 
     public Widget(int x, int y) {
         this.x = x;
         this.y = y;
-        count++;
-        System.out.println("Widget " + count + " created.");
+    }
+
+    public Widget(Widget w) {
+        this.x = w.x;
+        this.y = w.y;
     }
 
     public int getX() {
         return x;
     }
 
-    public static int getCount() {
-        return count;
+    public void setX(int x) {
+        this.x = x;
     }
+
 }
